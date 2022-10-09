83790a16b44399eb870af2740cdef76b4dfab2eb
Gestan <104741659+Gestan24@users.noreply.github.com>
2022-10-09 13:39:31 -0500

    added index.js to header file and updated app.js and index.css

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+// import Navagation from '../Navigation';
+
+function Header() {
+
+    return (
+
+        <div className="jumbotron">
+
+            <h1 className="display-1">
+                Gestan Joshua
+            </h1>
+
+            
+
+        </div>
+
+    );
+
+}
+
+
+export default Header;
